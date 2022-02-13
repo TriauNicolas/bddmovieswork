ae5fba7e83fff8d4e3d19eaf0bef74defdd4fb09
Nicolas triau <n_triau@hetic.eu>
2022-02-13 16:09:38 +0100

    another try

--- PDOCall.php
+++ PDOCall.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors', '0');
+
 $sakilaDB = new \PDO('mysql:host=' . $_ENV['MYSQL_HOST'] . ';port=' . $_ENV['MYSQL_PORT'] . ';dbname=' . $_ENV['MYSQL_DATABASE'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_ROOT_PASSWORD']);
 
 $limit = 10;
