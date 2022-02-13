7cc25755e86a7c644ef3819136d24798b7f47b84
Nicolas triau <n_triau@hetic.eu>
2022-02-13 14:49:02 +0100

    update for composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '233718661cf216f36d9495c316f3387ed5493570',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '233718661cf216f36d9495c316f3387ed5493570',
+            'dev_requirement' => false,
+        ),
+    ),
+);
